a78ac66119ea73a88c27f7cf5e5daefbf593167d
daorongxing <12044046+yanqingzheng@user.noreply.gitee.com>
2023-01-17 17:20:13 +0800

    controller控制层接口开发

--- xuecheng-plus-content/xuecheng-plus-content-api/src/test/java/com/xuecheng/content/api/XuechengPlusContentApiApplicationTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/test/java/com/xuecheng/content/api/XuechengPlusContentApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.xuecheng;
+package com.xuecheng.content.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class XuechengPlusBaseApplicationTests {
+class XuechengPlusContentApiApplicationTests {
 
     @Test
     void contextLoads() {
